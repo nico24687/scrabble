eb7f23edf241c52f8d1dfb4c1d6029736d6dc1f5
nico24687 <theonlynico@hotmail.com>
2017-10-18 11:04:07 -0600

    work done on scrabble so far

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,8 +1,23 @@
 class Scrabble
 
   def score(word)
-    1
+      return 0 if word == nil 
+      return 0 if word == ""
+      values =  word.chars.map do |letter|
+      point_values[letter.upcase]
+    end
+    values.inject(0, :+)
   end
+
+  def score_with_multipliers(word, letter_multipliers, world_multiplier = 1 )
+    bonus_points = 10 if word.length == 7 
+    values = word.chars.map.with_index do |letter, index|
+      point_values[letter.upcase] * letter_multipliers[index] * world_multiplier
+    end 
+    values.inject(0, :+)
+  end 
+
+ 
 
   def point_values
     {
@@ -12,7 +27,8 @@ class Scrabble
       "M"=>3, "N"=>1, "O"=>1, "P"=>3,
       "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
       "U"=>1, "V"=>4, "W"=>4, "X"=>8,
-      "Y"=>4, "Z"=>10
+      "Y"=>4, "Z"=>10, 
     }
   end
+
 end
--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -6,8 +6,39 @@ require 'pry'
 
 class ScrabbleTest < Minitest::Test
   def test_it_can_score_a_single_letter
-    skip
+    # skip
     assert_equal 1, Scrabble.new.score("a")
     assert_equal 4, Scrabble.new.score("f")
   end
+
+  def test_it_can_score_a_word
+    game = Scrabble.new
+    assert_equal 8, game.score("hello")
+  end 
+
+  def test_it_will_score_an_empty_string_with_zero
+    game = Scrabble.new 
+    assert_equal 0, game.score("")
+  end 
+
+  def test_it_can_score_nil_with_zero
+    game = Scrabble.new
+    assert_equal 0, game.score(nil)
+  end 
+
+  def test_can_score_word_with_letter_multipliers
+    game = Scrabble.new
+    assert_equal 9, game.score_with_multipliers('hello', [1,2,1,1,1])
+  end 
+
+  def test_can_score_word_with_word_multiplier
+    game = Scrabble.new
+    assert_equal 18, game.score_with_multipliers('hello', [1,2,1,1,1], 2)
+  end 
+
+  def test_can_score_a_word_with_seven_letter_bonus
+    game = Scrabble.new
+    assert_equal 58, game.score_with_multipliers('sparkle', [1,2,1,3,1,2,1], 2) 
+  end 
+
 end
